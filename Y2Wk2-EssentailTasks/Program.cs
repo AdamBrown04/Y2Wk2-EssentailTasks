6311881ef18ee8fa4decaa0cf2d5eb87753c3cb7
AdamBrown04 <adambrown2004@outlook.com>
2024-10-02 13:34:41 +0100

    completed the core of task 1, going to refine it to ensure a better program

--- Y2Wk2-EssentailTasks/Program.cs
+++ Y2Wk2-EssentailTasks/Program.cs
@@ -1,1 +1,65 @@
-﻿cons
+﻿using System;
+
+Console.WriteLine("What program do you want to run? \n1)guest list");
+int option = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+switch (option) 
+{
+    case 1:
+        List<string> guestList = new List<string>();
+
+        while (true)
+        {
+            Console.WriteLine("What would you like to do? \n1)Display guest list \n2)Add names to list \n3)Remove names \n4)How many people are on the list \n5)End program");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            if(userInput == 1)
+            {
+                foreach (string guest in guestList)
+                {
+                    Console.WriteLine($"{guest}");
+                }
+            }
+            else if (userInput == 2)
+            {
+                Console.Write("Enter a name you want to add: ");
+                guestList.Add(Console.ReadLine());
+            }
+            else if (userInput == 3)
+            {
+                Console.Write("Enter a name you want to remove: ");
+                string nameToRemove = Console.ReadLine();
+
+                bool found = false;
+                for(int i = 0; i < guestList.Count; i++)
+                {
+                    if (guestList[i] == nameToRemove)
+                    {
+                        guestList.RemoveAt(i);
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    Console.WriteLine($"{nameToRemove} was removed from the list");
+                }
+                else
+                {
+                    Console.WriteLine($"{nameToRemove} was not found in the list");
+                }
+            }
+            else if(userInput == 4)
+            {
+                Console.WriteLine($"There are {guestList.Count} guests on the list");
+            }
+            else if(userInput == 5)
+            {
+                break;
+            }
+
+        }
+        break;
+}
